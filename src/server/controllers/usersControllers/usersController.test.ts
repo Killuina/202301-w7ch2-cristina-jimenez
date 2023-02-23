83d1fa564e996fdb5b6056771fc40798284c814e
Cristina <cristinajmartin98@gmail.com>
2023-02-23 23:08:06 +0100

    Add test for calling json method of res on createUser

--- src/server/controllers/usersControllers/usersController.test.ts
+++ src/server/controllers/usersControllers/usersController.test.ts
@@ -4,6 +4,10 @@ import { type UserCredentials } from "../../../types";
 import { createUser } from "./usersControllers";
 
 describe("Given the createUser controller", () => {
+  const user = {
+    username: "hola",
+    password: "12345678",
+  };
   describe("When it receives a request with username: 'hola' and a password: '12345678", () => {
     test("Then it should call the status method with 201", async () => {
       const req = {
@@ -28,6 +32,29 @@ describe("Given the createUser controller", () => {
       await createUser(req, res as Response, next);
 
       expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("then it should call its json method with object with property user containing the given user", async () => {
+      const req = {
+        body: {
+          username: "hola",
+          password: "12345678",
+        },
+      } as Request<
+        Record<string, unknown>,
+        Record<string, unknown>,
+        UserCredentials
+      >;
+      const res: Partial<Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue(user),
+      };
+      const next = (() => {}) as NextFunction;
+      User.create = jest.fn().mockReturnValue(user);
+
+      await createUser(req, res as Response, next);
+
+      expect(res.json).toBeCalledWith({ user });
     });
   });
 });
--- src/server/routers/usersRouter/usersRouter.test.ts
+++ src/server/routers/usersRouter/usersRouter.test.ts
@@ -22,14 +22,10 @@ describe("Given the POST /users/login endpoint", () => {
     username: "Diana",
     password: "quehagoquehago",
   };
-
-  beforeAll(async () => {
-    await User.create(mockUser);
-  });
-
   describe("When it receives a request with a user with name 'Diana' and password 'quehagoquehago'", () => {
     test("Then it should respond with status 200 and token 'nosedemomento'", async () => {
       const expectedStatus = 200;
+      await User.create(mockUser);
 
       const response = await request(app)
         .post("/users/login")
